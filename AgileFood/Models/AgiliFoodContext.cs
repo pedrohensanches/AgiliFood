c706a65c2cde43473ee9e7e56cd0ef14e314e098
Pedro Henrique Sanches da Silva <pedrohenrique_pi@hotmail.com>
2018-05-29 04:09:31 -0300

    Criando Controllers e Views 1

---
+++ AgileFood/Models/AgiliFoodContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AgileFood.Models
+{
+    public class AgiliFoodContext : DbContext
+    {
+
+        public AgiliFoodContext() : base("name=AgiliFoodContext") { }
+
+        public DbSet<Cardapio> Cardapio { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Fornecedor> Fornecedor { get; set; }
+        public DbSet<ItemPedido> ItemPedido { get; set; }
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
