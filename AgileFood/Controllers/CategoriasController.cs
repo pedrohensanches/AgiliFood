56e38f74b6fde58a63ff0967760f9a80d224c68e
Pedro Henrique Sanches da Silva <pedrohenrique_pi@hotmail.com>
2018-05-30 19:02:58 -0300

    Criando Controllers e Views com Scaffolding

---
+++ AgileFood/Controllers/CategoriasController.cs
@@ -0,0 +1,22 @@
+﻿using AgileFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgileFood.Controllers
+{
+    public class CategoriasController : Controller
+    {
+
+        private AgiliFoodContext db = new AgiliFoodContext();
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            var categorias = db.Categorias;
+            return View(categorias.ToList());
+        }
+    }
+}
